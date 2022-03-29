e1ca1e46b99f1284cfd36a05933d95a6bcf976f0
byeong jae lee <redgpcmffld@gmail.com>
2022-03-29 14:08:10 +0900

    모의고사

---
+++ 220329_programmers_42840.py
@@ -0,0 +1,24 @@
+# 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다.
+# 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+#
+# 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+# 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+# 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+#
+# 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때,
+# 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+def solution(answers):
+    import math
+    picks = {1: [1, 2, 3, 4, 5], 2: [2, 1, 2, 3, 2, 4, 2, 5], 3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]}
+    hits = {1: 0, 2: 0, 3: 0}
+
+    for person in picks:
+        picks[person] = picks[person] * math.ceil(len(answers) / len(picks[person]))
+        for i in range(len(answers)):
+            if answers[i] == picks[person][i]:
+                hits[person] += 1
+
+    answer = [x for x in hits if hits[x] == max(hits.values())]
+
+    return answer
